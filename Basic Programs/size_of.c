e65cab06322a4b9afc41488fb60e823ad84950c2
Saniya <saniyam2k@gmail.com>
2023-03-26 20:23:26 +0530

    Basic Program

---
+++ Basic Programs/size_of.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <conio.h>
+ int main(){
+    char ch = '\0';
+    int No = 55;
+    printf("\n size for Integer No is=%d",sizeof(No));
+    printf("\n size for character variable ch is = %d",sizeof(ch));
+    printf("\n size for float data type is = %d",sizeof(float));
+ }
