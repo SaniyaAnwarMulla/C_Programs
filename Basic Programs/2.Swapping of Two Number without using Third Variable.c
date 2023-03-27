d73fa2614f6f3cf69d6e1c1c8b5ce8aeeb45734f
Saniya <saniyam2k@gmail.com>
2023-03-27 12:47:06 +0530

    Assignment No.1

---
+++ Basic Programs/2.Swapping of Two Number without using Third Variable.c
@@ -0,0 +1,17 @@
+//Swapping of Two Number Without Using Third Variable.
+#include<stdio.h>
+#include<conio.h>
+int main(){
+
+    int No1=0,No2=0;
+    printf("Enter Two Numbers:- ");
+    scanf("%d%d",&No1,&No2);
+
+    printf("\n Before Swapping => 1st Number=>%d, 2nd Number=>%d",No1,No2);
+    No1=No1+No2;
+    No2=No1-No2;
+    No1=No1+No2;
+    printf("\n After Swapping => 1st Number=>%d, 2nd Number=>%d",No1,No2);
+    getch();
+    return 0;
+}
