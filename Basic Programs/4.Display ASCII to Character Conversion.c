4102325d8da76ea6c5081d478ac406a30ee29116
Saniya <saniyam2k@gmail.com>
2023-03-29 20:41:47 +0530

    Basic Program

---
+++ Basic Programs/4.Display ASCII to Character Conversion.c
@@ -0,0 +1,14 @@
+//Display ASCII to Character Conversion
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int No=0;
+  printf("\n ASCII to Characer Conversion=>\n\n");
+  printf("\n Enter ASCII value to get It's corresponding character=> ");
+  scanf("%d",&No);
+  printf("\n\n Character for Given ASCII value %d is = |%c|.",No,No);
+  printf("\n\n Thanks for using our Services..!");
+  getch();
+  return 0;
+}
