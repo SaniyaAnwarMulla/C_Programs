6a47d791651ee27261c9e6ddb0650e96647b11e9
Saniya <saniyam2k@gmail.com>
2023-04-03 15:55:36 +0530

    Basic Program

---
+++ Basic Programs/8.Accept 2 Numbers from user to display Even & Odd.c
@@ -0,0 +1,19 @@
+//Accept 2 Numbers from user to display Even & Odd.
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int Num=0;
+    printf("\n Enter an Integer Number: ");
+    scanf("%d",&Num);
+    if(Num%2==0)
+    {
+        printf("\n Given Number is Even");
+    }
+    else
+    {
+        printf("\n Given Number is Odd");
+    }
+    getch();
+    return 0;
+}
