3c34fe3da4de9a388d425534c3d696c1c776db6b
Saniya <saniyam2k@gmail.com>
2023-04-05 20:37:40 +0530

    Basic Program

---
+++ Basic Programs/10.Accept 2 Numbers from user to display Even & Odd (Using Ternary within Ternary).c
@@ -0,0 +1,13 @@
+//Accept 2 Numbers from user to display Even & Odd (Using Ternary within Ternary).
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int Num=0;
+    printf("\n Enter an Integer Number: ");
+    scanf("%d",&Num);
+    (Num==0)?printf("\n Given Number is Neutral."):(Num%2==0)?printf("\n Given Number is Even."):printf("\n Given Number is Odd.");
+    getch();
+    return 0;
+
+}
